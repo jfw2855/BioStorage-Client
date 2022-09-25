59b155d136d3b78c8ffd77e38daf9733c8d7365e
jfw2855 <jwaddle1130@gmail.com>
2022-09-25 15:40:40 -0500

    adds routes to signup, signin, and update password pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import React, { useState, Fragment, switddfsdf } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import UpdatePassword from './components/UpdatePassword';
 
-function App() {
+const App = () => {
+
+  const [user,setUser] = useState(null)
   return (
-    <div className="App">
-      test
-    </div>
+      <>
+        <Fragment>
+          <Routes>
+            <Route path='/'/>
+            <Route path='/sign-in' element={<SignIn setUser={setUser}/>}/>
+            <Route path='/sign-up' element={<SignUp setUser={setUser}/>}/>
+            <Route path='/update-password' element={<UpdatePassword/>}/>
+          </Routes>
+        </Fragment>
+
+      </>
+
   );
 }
 
 export default App;
+
