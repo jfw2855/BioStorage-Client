ca21eb42f4ac34fc6c0480ff195f33c3cdbe1236
jfw2855 <jwaddle1130@gmail.com>
2022-10-05 12:40:50 -0500

    adds requiredAuth component to protect auth routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState, Fragment, switddfsdf } from 'react'
+import React, { useState, Fragment } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Header from './components/Header';
+import RequireAuth from './components/shared/RequireAuth';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import UpdatePassword from './components/UpdatePassword';
+import Home from './components/Home';
 
 const App = () => {
 
@@ -12,10 +13,19 @@ const App = () => {
       <>
         <Fragment>
           <Routes>
-            <Route path='/'/>
+            {/* unauthenticated routes */}
             <Route path='/sign-in' element={<SignIn setUser={setUser}/>}/>
             <Route path='/sign-up' element={<SignUp setUser={setUser}/>}/>
             <Route path='/update-password' element={<UpdatePassword/>}/>
+            {/* {authenticated routes} */}
+            <Route 
+						path='/'
+						element={
+						<RequireAuth user={user}>
+							<Home user={user} />
+						</RequireAuth>
+						} 
+						/>
           </Routes>
         </Fragment>
 
